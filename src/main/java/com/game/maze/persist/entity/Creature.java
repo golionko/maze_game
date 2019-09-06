33fa759796cde2397c0bd380079e46f8605bada3
golionko <golionko@gmail.com>
2019-09-06 15:30:35 -0400

    maze creatures

---
+++ src/main/java/com/game/maze/persist/entity/Creature.java
@@ -0,0 +1,34 @@
+package com.game.maze.persist.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "creature")
+public class Creature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long xp;
+    private Long level;
+    private Long strength;
+    private Long dexterity;
+    private Long luck;
+    private Long constitution;
+    private Long hp;
+    @Column(name = "max_hp")
+    private Long maxHp;
+    private Long energy;
+    @Column(name = "max_energy")
+    private Long maxEnergy;
+    @Column(name = "labyrinth_room_id")
+    private Long roomId;
+}
